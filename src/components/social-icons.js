03a0f8c8128f3ecaa2bb2c160f504c3a273145d2
Kevin Jones <kevin.jones@ppushdr.com>
2020-09-19 15:07:07 +0100

    replaced needless div with a React.Fragment

--- src/components/social-icons.js
+++ src/components/social-icons.js
@@ -2,58 +2,42 @@ import React from "react"
 import { SocialIcon } from "react-social-icons"
 
 const SocialIcons = () => {
+  const icons = [
+    {
+      url: "http://twitter.com/kevcjones",
+      network: "twitter",
+    },
+    {
+      url: "https://www.linkedin.com/in/kevin-c-jones-7a22372/",
+      network: "linkedin",
+    },
+    {
+      url: "https://github.com/kevcjones",
+      network: "github",
+    },
+    {
+      url: "https://t.me/codebykev",
+      network: "whatsapp",
+    },
+  ]
   return (
-    <div>
-      <SocialIcon
-        style={{
-          height: 25,
-          width: 25,
-          marginRight: "0.5em",
-          marginTop: "1em",
-        }}
-        bgColor="#3182ce"
-        fgColor="#fff"
-        url="http://twitter.com/kevcjones"
-        className="duration-150 ease-in-out transform hover:-translate-y-1 hover:scale-125"
-      />
-      <SocialIcon
-        style={{
-          height: 25,
-          width: 25,
-          marginRight: "0.5em",
-          marginTop: "1em",
-        }}
-        bgColor="#3182ce"
-        fgColor="#fff"
-        url="https://www.linkedin.com/in/kevin-c-jones-7a22372/"
-        className="duration-150 ease-in-out transform hover:-translate-y-1 hover:scale-125"
-      />
-      <SocialIcon
-        style={{
-          height: 25,
-          width: 25,
-          marginRight: "0.5em",
-          marginTop: "1em",
-        }}
-        bgColor="#3182ce"
-        fgColor="#fff"
-        url="https://github.com/kevcjones"
-        className="duration-150 ease-in-out transform hover:-translate-y-1 hover:scale-125"
-      />
-      <SocialIcon
-        style={{
-          height: 25,
-          width: 25,
-          marginRight: "0.5em",
-          marginTop: "1em",
-        }}
-        network="whatsapp"
-        bgColor="#3182ce"
-        fgColor="#fff"
-        url="https://t.me/codebykev"
-        className="duration-150 ease-in-out transform hover:-translate-y-1 hover:scale-125"
-      />
-    </div>
+    <React.Fragment>
+      {icons.map(icon => (
+        <SocialIcon
+          style={{
+            height: 25,
+            width: 25,
+            marginRight: "0.5em",
+            marginTop: "1em",
+          }}
+          bgColor="#3182ce"
+          fgColor="#fff"
+          network={icon.network}
+          url={icon.url}
+          className="duration-150 ease-in-out transform hover:-translate-y-1 hover:scale-125"
+        />
+      ))}
+    </React.Fragment>
   )
 }
 
