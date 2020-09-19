7d61e3d68f1838e89035d46557f0079e3889155f
Kevin Jones <kevin.jones@ppushdr.com>
2020-09-19 01:07:25 +0100

    remove sleep input

--- src/components/profile-image.js
+++ src/components/profile-image.js
@@ -10,7 +10,6 @@ const trans = (x, y, s) =>
   `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
 
 const ProfileImage = () => {
-  ss
   const [props, set] = useSpring(() => ({
     xys: [0, 0, 1],
     config: { mass: 5, tension: 350, friction: 40 },
