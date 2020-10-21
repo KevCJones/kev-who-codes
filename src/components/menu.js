78b83eab7003716b515c3270745c34e542a6c10a
Kevin Jones <kevin.jones@ppushdr.com>
2020-10-21 21:40:01 +0100

    update link to blog

--- src/components/menu.js
+++ src/components/menu.js
@@ -5,7 +5,7 @@ import Button from "./button"
 const MainMenu = () => {
   return (
     <div className="flex flex-col mt-6 md:flex-row">
-      <a href="https://medium.com/@codebykev" target="_blank" rel="noreferrer">
+      <a href="https://codebykev.medium.com" target="_blank" rel="noreferrer">
         <Button className="p-3 mt-4 md:mr-4 md:mt-0 border-secondary text-secondary hover:bg-secondary hover:text-white">
           Personal Blog
         </Button>
